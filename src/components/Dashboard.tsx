8f3b9362126bb4fc882fff0d728fdc160b938d71
AditiJ11 <111302741+AditiJ11@users.noreply.github.com>
2024-06-08 19:43:29 +0530

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -38,7 +38,7 @@ const Dashboard: React.FC = () => {
   const [graphType, setGraphType] = useState<string>('All');
 
   useEffect(() => {
-    fetch('https://dd10.onrender.com')
+    fetch('https://dd10.onrender.com/data')
       .then(response => response.json())
       .then(data => {
         setData(data.AuthorWorklog.rows);
